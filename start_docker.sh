a8f8b8b9fd4d626d5045091a0c13ebf17a108226
isa-conde <isconde@itba.edu.ar>
2025-04-04 14:03:32 -0300

    add to my script

--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,7 @@
-gcc player.c -Wall -o player
-gcc vista.c -Wall -o vista
+gcc -g -O0 player.c -Wall -o player
+gcc -g -O0 vista.c -Wall -o vista
+gcc -g -O0 maxPlayer.c -Wall -o MAX
 docker run -v "${PWD}:/root" --privileged -ti agodio/itba-so-multi-platform:3.0
+
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --trace-children=yes ./ChompChamps
+getopt
