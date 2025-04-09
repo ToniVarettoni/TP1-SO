b338a63725e38c958f000c8025d6e19e38c512a7
ToniVarettoni <tvarettoni@itba.edu.ar>
2025-04-08 22:37:30 -0300

    master move reading and procesing

--- master.c
+++ master.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>  
 #include <unistd.h>
 #include <time.h>
+#include <sys/select.h>
 
 #define ARGUMENT_ERROR "Usage: ./master [-w width] [-h height] [-d delay] [-s seed] [-v view] [-t timeout] -p player1 player2 ..."
 
@@ -23,7 +24,11 @@ int initPlayer(GameState * gameState, int i);
 void spawnPlayer(GameState * gameState, int i);
 void updateMap(GameState * gameState, int index);
 void setMap(GameState * gameState, unsigned int seed);
-void get_moves();
+void get_moves(int playerPipes[]);
+void updateMoves(unsigned char moves[MAX_PLAYERS], GameState * gameState);
+bool processMove(GameState * gameState, int currentPlayer, unsigned char move);
+bool checkCantMove(GameState * gameState, int currentPlayer);
+
 
 int main(int argc, char *argv[]) {
 
@@ -106,7 +111,7 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
     }
 
-    if (sem_init(&syncState->stateSem, 1, 0) == -1) {
+    if (sem_init(&syncState->stateSem, 1, 1) == -1) {
         perror("Error initializing stateSem");
         exit(EXIT_FAILURE);
     }
@@ -143,21 +148,76 @@ int main(int argc, char *argv[]) {
     gameState->width = width;
 
     checkArguments(gameState); 
+
+    int currentPlayer = 0;
+    int playingPlayers = gameState->playerAmount;
     
-    while(1){
+    while(playingPlayers > 0){
         sem_post(&syncState->readyToPrint);
         sem_wait(&syncState->printDone);
 
-        // get_moves();
-
+        sem_wait(&syncState->masterSem);
         sem_wait(&syncState->stateSem);
-        // updateMoves();
-        sem_post(&syncState->stateSem);
+        sem_post(&syncState->masterSem);
+
+
+        currentPlayer = ++currentPlayer % gameState->playerAmount;
+        
+        int fd = playerPipes[currentPlayer];
+
+        if (fd == -1)
+            continue;
+        
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(fd, &readfds);
+
+        struct timeval timeout = {0, 10};
+
+        int ready = select(fd + 1, &readfds, NULL, NULL, &timeout);
+    
+        if (ready < 0 ){
+            perror("select");
+            break;
+        }
+        
+
+
+        if (ready > 0 && FD_ISSET(fd, &readfds)){
+            unsigned char move;
+            int n = read(fd, &move, sizeof(move));
+            
+            if (n <= 0){
+                close(fd);
+                playerPipes[currentPlayer] = -1;
+                playingPlayers--;
+            }else{
+
+
+
+                if(processMove(gameState, currentPlayer, move) == 1){
+                    if (checkCantMove(gameState, currentPlayer)){
+                        gameState->players[currentPlayer].cantMove = true;
+                        playingPlayers--;
+                    }
+                    
+                    updateMap(gameState, currentPlayer);
+                }
+
+            }
+            sem_post(&syncState->stateSem);
+            sleep(1);
+            
+        }
+        
+        
+        
+
     }
 
 }
 
-void get_moves(){
+void get_moves(int playerPipes[]){
     fd_set readfds;
     int maxfd = 0;
     unsigned char moves[MAX_PLAYERS];
@@ -169,10 +229,10 @@ void get_moves(){
     }
 }
 
-void updateMoves(unsigned char moves[MAX_PLAYERS]){
+void updateMoves(unsigned char moves[MAX_PLAYERS], GameState * gameState){
     for(size_t i = 0; i < MAX_PLAYERS; i++){
         switch(moves[i])
-        gameState->player[i].x
+        gameState->players[i].x;
     }
 }
 
@@ -237,15 +297,16 @@ int initPlayer(GameState * gameState, int i){
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO);
 
-        char * path = strcat("./", gameState->players[i].name);
+        char path[MAX_LENGTH_PATH];
+        strcpy(path, "./"); 
+        strcat(path, gameState->players[i].name);
         char height[MAX_LENGTH_NUM];
         char width[MAX_LENGTH_NUM];
 
         sprintf(height, "%d", gameState->height);
         sprintf(width, "%d", gameState->width);
 
-
-        char *args[] = {path, height, width, NULL };
+        char *args[] = {path, height, width, NULL};        
         execve(path, args, NULL);
         perror("execve player");
         exit(EXIT_FAILURE);
@@ -303,6 +364,7 @@ void spawnPlayer(GameState * gameState, int i){
 }
 
 void updateMap(GameState * gameState, int index){
+    gameState->players[index].score += gameState->map[gameState->players[index].x + gameState->players[index].y * (gameState->width)];
     gameState->map[gameState->players[index].x + gameState->players[index].y * (gameState->width)] = 0 - index;
 }
 
@@ -315,3 +377,94 @@ void setMap(GameState * gameState, unsigned int seed){
     }
 }
 
+bool processMove(GameState * gameState, int currentPlayer, unsigned char move){
+
+
+    int x = gameState->players[currentPlayer].x;
+    int y = gameState->players[currentPlayer].y;
+    int w = gameState->width;
+    int h = gameState->height;
+
+    switch (move){
+    case 0:
+        if (y > 0){
+            gameState->players[currentPlayer].y--;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break;
+
+    case 1:
+        if (y > 0 && x < w-1){
+            gameState->players[currentPlayer].x++;
+            gameState->players[currentPlayer].y--;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break;
+    case 2:
+        if (x < w-1){
+            gameState->players[currentPlayer].x++;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break;
+    case 3:
+        if (x < w-1 && y < h-1){
+            gameState->players[currentPlayer].x++;
+            gameState->players[currentPlayer].y++;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break;
+    case 4:
+        if (y < h-1){
+            gameState->players[currentPlayer].y++;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break;
+    case 5:
+        if (x > 0 && y < h-1){
+            gameState->players[currentPlayer].x--;
+            gameState->players[currentPlayer].y++;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break; 
+    case 6: 
+        if (x > 0){
+            gameState->players[currentPlayer].x--;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break;
+    case 7:
+        if (x > 0 && y > 0){
+            gameState->players[currentPlayer].x--;
+            gameState->players[currentPlayer].y--;
+            gameState->players[currentPlayer].validMoves++;
+            return true;
+        }break; 
+    default:
+        gameState->players[currentPlayer].invalidMoves++;
+        return false;
+    }
+    
+    gameState->players[currentPlayer].invalidMoves++;
+    return false;
+}
+
+
+bool checkCantMove(GameState * gameState, int currentPlayer){
+
+    int x = gameState->players[currentPlayer].x;
+    int y = gameState->players[currentPlayer].y;
+    int w = gameState->width;
+    int h = gameState->height;
+
+    for (int i = x - 1; i <= x + 1; i++){
+        for (int j = y - 1; j <= y + 1; j++){
+            if (i >= 0 && j >= 0 && i < w && j < h){
+                if (gameState->map[i + w*j] > 0){
+                    return !true;                   //UN POCO CONFUSO PERO ESTA FUNCION TIENE MAS SENTIDO
+                }
+            }
+        }    
+    }
+    return !false;
+
+}
